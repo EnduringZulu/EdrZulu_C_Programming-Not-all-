3f3f87b573368b4d96b3366d9af62ee86ff9931a
EnduringZulu <mingrattikorn@gmail.com>
2023-08-16 19:35:53 +0700

    q1_if-else

---
+++ w3resource/conditional st/q1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int x,y;
+    scanf("%d",&x);
+    scanf("%d",&y);
+    if (x==y)
+    {
+        printf("Number1 and Number2 are equal");
+    }
+    else
+    {
+        printf("Number1 and Number2 are not equal");
+    }
+    return 0;
+}
