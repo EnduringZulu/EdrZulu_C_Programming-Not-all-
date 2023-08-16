0fe38f4c305d7bba60d5525965cf66ddfa790721
EnduringZulu <mingrattikorn@gmail.com>
2023-08-16 20:10:33 +0700

    q22 else-if

---
+++ w3resource/conditional st/q22/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(){
+    int inp;
+    scanf("%d",&inp);
+    if (inp == 1)
+    {
+        printf("One");
+    }
+    else if (inp == 2)
+    {
+        printf("Two");
+    }
+    else if (inp == 3)
+    {
+        printf("Three");
+    }
+    else if (inp == 4)
+    {
+        printf("Four");
+    }
+    else if (inp == 5)
+    {
+        printf("Five");
+    }
+    else if (inp == 6)
+    {
+        printf("Six");
+    }
+    else if (inp == 7)
+    {
+        printf("Seven");
+    }
+    else if (inp == 8)
+    {
+        printf("Eight");
+    }
+    else if (inp == 9)
+    {
+        printf("Nine");
+    }
+    else if (inp == 10)
+    {
+        printf("Ten");
+    }
+
+    return 0;
+}
