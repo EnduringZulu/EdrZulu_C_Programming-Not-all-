55d7629ac1f1a04f66ecd4493b7ae376aa3aa8ea
EnduringZulu <mingrattikorn@gmail.com>
2023-08-20 22:32:15 +0700

    reverse xtra_09

--- Xtra_week1/Xtra_09/main.c
+++ Xtra_week1/Xtra_09/main.c
@@ -2,16 +2,11 @@
 
 int main(){
     float inp;
-    printf("Input:");
+    printf("Input: ");
     scanf("%f",&inp);
     int result_1 = (inp * 100)/100;
-    int result_2 = ((inp)-result_1)*1000000;
-    while (result_2 % 10 == 0)
-    {
-        result_2 = result_2 / 10;
-    }
-    
-    printf("Whole Number: %d\n",result_1);
-    printf("Decimal places: %d",result_2);
+    float result_2 = ((inp)-result_1) * 100;
+    printf("Whole Number: %d\n", result_1);
+    printf("Decimal places: %.0f", result_2);
     return 0;
 }
