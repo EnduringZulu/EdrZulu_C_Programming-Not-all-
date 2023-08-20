234b880c75803c5eb2b5e4c46df85ede58c82db4
EnduringZulu <mingrattikorn@gmail.com>
2023-08-20 20:15:41 +0700

    updated q10

--- Jode/q10/main.c
+++ Jode/q10/main.c
@@ -7,6 +7,12 @@ int main()
     int hr = sec / 3600;
     int min = (sec % 3600)/60;
     sec = (sec%3600)%60;
-    printf("%d:%d:%d",hr,min,sec);
+    if(min<=9||sec<=9){
+        printf("%d:0%d:0%d",hr,min,sec);
+    }
+    else{
+        printf("%d:%d:%d", hr, min, sec);
+    }
+    
     return 0;
 }
