f0e90a72a9d9726d07de332d91c2f60750ef80ba
EnduringZulu <mingrattikorn@gmail.com>
2023-08-21 19:45:12 +0700

    Fibonacci

--- ankitweblogic/qFibonacci/main.c
+++ ankitweblogic/qFibonacci/main.c
@@ -2,5 +2,18 @@
 #include <stdio.h>
 
 int main(){
+    int n;
+    int a = 0, b=1,result;
+    scanf("%d", &n);
+    printf("0 ");
+    printf("1 ");
+    for ( int i = 0; i < n-2; i++)
+    {
+        result = a + b;
+        printf("%d ", result);
+        a = b;
+        b = result;
+    }
+    
     return 0;
 }
