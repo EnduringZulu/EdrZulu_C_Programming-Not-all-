311195cd977fb62ee299197f8b1524fefd365404
EnduringZulu <mingrattikorn@gmail.com>
2023-08-27 19:19:53 +0700

    Sheet&Xtra_w3_q3-2

--- Xtra_week3/q3_2/main.c
+++ Xtra_week3/q3_2/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
+    char inp[10];
+    int count=0;
+    float sum=0;
     
+    while(inp[0]!='q'){
+        scanf("%s", &inp);
+        if(inp[0]!='q'&& atoi(inp)>=0&&atoi(inp)<=100){
+            count+=1;
+            sum = sum + atoi(inp);
+        }
+    }
+    printf("%.2f ",sum/count);
+    if(sum>50){
+        printf("Pass");
+    }
+    else if(sum<=50&&sum>=0){
+        printf("Fail");
+    }
     return 0;
 }
