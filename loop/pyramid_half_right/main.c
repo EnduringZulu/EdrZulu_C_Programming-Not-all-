073eaa44932a56e4d57571636285f24dd685212c
EnduringZulu <mingrattikorn@gmail.com>
2023-08-29 21:20:03 +0700

    pyramid

--- loop/pyramid_half_right/main.c
+++ loop/pyramid_half_right/main.c
@@ -2,24 +2,6 @@
 /*
 int main(){
     int n;
-    scanf("%d",&n);
-    for(int i=0;i<n*2;i++){
-        for(int j=0;j<n;j++){
-            if(i+j>=n-1){
-                printf("*");
-            }
-            else{
-                printf(" ");
-            }
-        }
-        printf("\n");
-    }
-    return 0;
-}
-*/
-/*
-int main(){
-    int n;
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
@@ -38,9 +20,9 @@ int main(){
     scanf("%d",&n);
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < (n * 2) - 1; j++)
+        for (int j = 0; j < (n*2)-1; j++)
         {
-            if(j-1==n-1){
+            if(i+j>=n-1&&j-i<=n-1){
                 printf("*");
             }
             else{
