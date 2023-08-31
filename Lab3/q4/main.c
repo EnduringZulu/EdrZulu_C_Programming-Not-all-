319f9849644b47d643bb698a17ceb911ec58b417
EnduringZulu <mingrattikorn@gmail.com>
2023-08-31 14:19:22 +0700

    fixed lab3q4

--- Lab3/q4/main.c
+++ Lab3/q4/main.c
@@ -6,14 +6,13 @@ int main()
     int count=0,count2=0,count3=0;
     while(x!='q'){
         scanf("%c", &x);
-        if(x%2!=0){
+        if(x%2!=0&&x!='q'){
             count = count + 1;
             count2 = (int)x-48;
-            printf("%d", count2);
+            count3 = count3 + count2;
         }
-        count3 = count3 + count2;
-        printf("\n%d", count3);
+        
     }
-    printf("%d:%d",count-1,count3);
+    printf("%d:%d",count,count3);
     return 0;
 }
