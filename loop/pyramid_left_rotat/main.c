2d2817f14d68ff7b3deb767238a76670020463b7
EnduringZulu <mingrattikorn@gmail.com>
2023-09-04 21:51:18 +0700

    loop_pyramid_left

---
+++ loop/pyramid_left_rotat/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    int row;
+    scanf("%d",&row);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            if (i >= j)
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            if (i <= j)
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
