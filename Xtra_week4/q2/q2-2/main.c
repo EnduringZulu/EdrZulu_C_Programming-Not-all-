93a473842f37b74b03b7c6feecc132a1d5bd1945
EnduringZulu <mingrattikorn@gmail.com>
2023-09-04 23:18:50 +0700

    q1-2

---
+++ Xtra_week4/q2/q2-2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    //x=1st y=2nd z=distance between num
+    /*int x,y,z;
+    scanf("%d %d %d",&x,&y,&z);
+    for(int i=x;i<=y;i+=z){
+        printf("%d ",i);
+    }*/
+    for (int i = -18; i <= 18; i += 3)
+    {
+        printf("%d ", i);
+    }
+    return 0;
+}
