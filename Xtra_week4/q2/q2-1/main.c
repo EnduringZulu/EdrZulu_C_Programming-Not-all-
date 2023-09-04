1a2615c81e3b91df89d3fb88d9b810d2836518db
EnduringZulu <mingrattikorn@gmail.com>
2023-09-04 23:14:08 +0700

    q2-1

---
+++ Xtra_week4/q2/q2-1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    /*int x,y;
+    for(int i=x;i<=y;i++){
+        printf("%d ",i);
+    }*/
+    for (int i = 4; i <= 10; i++)
+    {
+        printf("%d ", i);
+    }
+    return 0;
+}
