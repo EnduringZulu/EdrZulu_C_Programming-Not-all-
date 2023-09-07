1e7affa5997dfd69c87c8149f1d78f16830b5a60
EnduringZulu <mingrattikorn@gmail.com>
2023-09-07 14:28:24 +0700

    Lab5-q2

--- Lab5/q2/main.c
+++ Lab5/q2/main.c
@@ -4,13 +4,8 @@ int main(){
     int x;
     scanf("%d",&x);
     for(int i=0;i<x;i++){
-        for(int j=0;j<x;j++){
-            if(i>=j){
-                printf("*");
-            }
-            else{
-                printf(" ");
-            }
+        for(int j=0;j<=i;j++){
+            printf("*");
         }
         printf("\n");
     }
