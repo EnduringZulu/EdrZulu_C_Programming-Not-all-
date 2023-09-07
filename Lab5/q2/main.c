7d5525420975af8f4bead8cd568ae43fe123dc6a
EnduringZulu <mingrattikorn@gmail.com>
2023-09-07 14:55:58 +0700

    Lab5-q2

--- Lab5/q2/main.c
+++ Lab5/q2/main.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int x;
-    scanf("%d",&x);
-    for(int i=0;i<x;i++){
-        for(int j=0;j<=i;j++){
-            printf("*");
+    scanf("%d", &x);
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if(i%2==0){
+                if (j % 2 == 1)
+                {
+                    printf("0 ");
+                }
+                else
+                {
+                    printf("1 ");
+                }
+            }
+            else{
+                if (j % 2 == 0)
+                {
+                    printf("0 ");
+                }
+                else
+                {
+                    printf("1 ");
+                }
+            }
+            
         }
         printf("\n");
     }
