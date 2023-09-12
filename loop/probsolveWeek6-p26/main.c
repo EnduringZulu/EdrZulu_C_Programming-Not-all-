9aca8f5e5f577e3bf6686df00bf4f14fde285ae6
EnduringZulu <mingrattikorn@gmail.com>
2023-09-12 22:38:09 +0700

    ProbsolvNo2

--- loop/probsolveWeek6-p26/main.c
+++ loop/probsolveWeek6-p26/main.c
@@ -44,9 +44,30 @@ void No2(int n){
     }
 }
 
+void No3(int n){
+    // int n;
+    // i+j>=n-1 && j-i<=n-1
+    int k=n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(j>=k-1){
+                printf("* ");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        k--;
+        printf("\n");
+    }
+}
+
+
 int main(){
     No1(5);
     printf("\n");
     No2(5);
+    printf("\n");
+    No3(10);
     return 0;
 }
