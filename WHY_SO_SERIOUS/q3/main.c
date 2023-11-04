e7bbee3bf8c547be91d89884554dbf66f2e011dc
EnduringZulu <mingrattikorn@gmail.com>
2023-11-05 02:23:05 +0700

    not-finished

---
+++ WHY_SO_SERIOUS/q3/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void roman_sym(int n){
+    int M_ = n/1000000;
+    int D_ = n/500000;
+    int C_ = 100000;
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    roman_sym(n);
+    return 0;
+}
